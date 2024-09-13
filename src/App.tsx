51754a32c90357c50ad562060c489d5822be3a67
Erlangga <erradzan@gmail.com>
2024-09-13 16:40:39 +0700

    module 5

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecipeApp from './Main';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <RecipeApp />
+    </div>
+  );
+};
+
+export default App;
